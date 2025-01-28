119f8233a523cc0d71d828ce119dd0a1cdd37656
Rushi <rushin.mailme@gmail.com>
2025-01-29 00:19:32 +0530

    JS Project Setup

---
+++ assets/scripts/app.js
@@ -0,0 +1,65 @@
+let curResult = 0;
+let curCalculation = '0';
+
+
+addBtn.addEventListener('click', function() {
+    let userInput = GetInput();
+    if (!isNaN(userInput)) {
+        OperateAndPrintResult('+', userInput);
+    }
+});
+
+subtractBtn.addEventListener('click', function() {
+    let userInput = GetInput();
+    if (!isNaN(userInput)) {
+        OperateAndPrintResult('-', userInput);
+    }
+});
+
+multiplyBtn.addEventListener('click', function() {
+    let userInput = GetInput();
+    if (!isNaN(userInput)) {
+        OperateAndPrintResult('*', userInput);
+    }
+});
+
+divideBtn.addEventListener('click', function() {
+    let userInput = GetInput();
+    if (!isNaN(userInput)) {
+        if (userInput != 0) {
+            OperateAndPrintResult('/', userInput);
+        } else {
+            toastr.options.positionClass = 'toast-top-center';
+            toastr.options.progressBar = true;
+            toastr.options.timeOut = '1000'
+            toastr.warning('Cannot divide by 0');
+        }
+    }
+});
+
+function OperateAndPrintResult(operation, userInput) {
+    switch(operation) {
+        case '+':
+            curResult += parseFloat(userInput);
+            break;
+        case '-':
+            curResult -= parseFloat(userInput);
+            break;
+        case '*':
+            curResult *= parseFloat(userInput);
+            break;
+        case '/':
+            curResult /= parseFloat(userInput);
+            break;
+    }
+    curCalculation = `(${curCalculation} ${operation} ${userInput})`;
+    outputResult(curResult, curCalculation);
+}
+
+function GetInput() {
+    return userInput.value;
+}
+
+function Add (num1, num2) {
+    return num1 + num2;
+}
