ed45fef04b5bd9f619d7b852464037844d8f2da0
Rushi <rushin.mailme@gmail.com>
2025-01-30 01:02:49 +0530

    Refactor, Documentation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,13 @@
 let curResult = 0;
 let curCalculation = '0';
 
+// Function references in Buttons
 addBtn.addEventListener('click', Add);
 subtractBtn.addEventListener('click', Subtract);
 multiplyBtn.addEventListener('click', Multiply);
 divideBtn.addEventListener('click', Divide);
 
+// Functions
 function Add() {
     let userInput = GetInput();
     if (!isNaN(userInput)) {
@@ -41,6 +43,7 @@ function Divide() {
     }
 }
 
+// Internal Operations
 function OperateAndPrintResult(operation, userInput) {
     switch(operation) {
         case '+':
@@ -62,8 +65,4 @@ function OperateAndPrintResult(operation, userInput) {
 
 function GetInput() {
     return userInput.value;
-}
-
-function Add (num1, num2) {
-    return num1 + num2;
 }
